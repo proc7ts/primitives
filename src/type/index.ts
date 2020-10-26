65336067b1d45c3969b656c64aef757b915dcd50
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-10-26 20:15:57 +0700

    Implement `isPromiseLike` check

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,3 +1,4 @@
+export * from './is-array-like';
 export * from './is-iterable';
 export * from './is-array-like';
 export * from './presence';
---
+++ src/type/is-promise-like.spec.ts
@@ -0,0 +1,31 @@
+import { noop } from '../fn';
+import { isPromiseLike } from './is-promise-like';
+
+describe('isPromiseLike', () => {
+  it('succeeds for promise', () => {
+    expect(isPromiseLike(Promise.resolve())).toBe(true);
+  });
+  it('succeeds for then-able function', () => {
+
+    const thenable: (() => undefined) & PromiseLike<void> = () => void 0;
+
+    thenable.then = (ok, fail) => Promise.resolve().then(ok, fail);
+
+    expect(isPromiseLike(thenable)).toBe(true);
+  });
+  it('fails for `null`', () => {
+    expect(isPromiseLike(null)).toBe(false);
+  });
+  it('fails for non-object value', () => {
+    expect(isPromiseLike('abc')).toBe(false);
+  });
+  it('fails for plain function', () => {
+    expect(isPromiseLike(noop)).toBe(false);
+  });
+  it('fails for plain object', () => {
+    expect(isPromiseLike({ foo: 'bar' })).toBe(false);
+  });
+  it('fails for non-function `then` property', () => {
+    expect(isPromiseLike({ then: 1 })).toBe(false);
+  });
+});
