c0cc3c8f734e616a569944d13bfaf7d3e4694d87
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-05-14 12:36:16 +0700

    Add `hasOwnProperty()` utility function

---
+++ src/object/has-own-property.spec.ts
@@ -0,0 +1,33 @@
+import { hasOwnProperty } from './has-own-property';
+
+describe('hasOwnProperty', () => {
+  it('returns `true` when target object owns property', () => {
+
+    const object: { a?: string } = Object.create(null);
+
+    object.a = undefined;
+
+    expect(hasOwnProperty(object, 'a')).toBe(true);
+  });
+  it('returns `false` when target property deleted', () => {
+
+    const object: { a?: string } = { a: 'test' };
+
+    delete object.a;
+
+    expect(hasOwnProperty(object, 'a')).toBe(false);
+  });
+  it('returns `false` for the method defined in prototype', () => {
+
+    class Base {
+
+      a(): string {
+        return 'test';
+      }
+
+    }
+
+    expect(hasOwnProperty(new Base(), 'a')).toBe(false);
+    expect(hasOwnProperty(Base.prototype, 'a')).toBe(true);
+  });
+});
