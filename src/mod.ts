40d72c7655906b3444e585d686120c973e0bf2fd
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-05-21 10:21:32 +0700

    Move promise resolver to `@proc7ts/async`

--- src/mod.ts
+++ src/mod.ts
@@ -6,6 +6,5 @@ export * from './array/mod.js';
 export * from './class/mod.js';
 export * from './fn/mod.js';
 export * from './object/mod.js';
-export * from './promises/mod.js';
 export * from './value/mod.js';
 export * from './type/mod.js';
