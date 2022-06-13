ee4d2955ce290e28b40a6e73df0ec482f2168ddd
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-06-13 09:55:40 +0700

    fix: proper `isIterable()` check on primitive values

--- src/type/is-iterable.spec.ts
+++ src/type/is-iterable.spec.ts
@@ -6,10 +6,24 @@ describe('isIterable', () => {
   it('returns `true` for array', () => {
     expect(isIterable([])).toBe(true);
   });
+  it('returns `true` for iterable object', () => {
+    expect(isIterable(new Set().values())).toBe(true);
+  });
+  it('returns `true` for iterable function', () => {
+    const value = (() => void 0) as Partial<Iterable<number>>;
+
+    value[Symbol.iterator] = function *() { yield* [1, 2, 3]; };
+
+    expect(isIterable(value)).toBe(true);
+  });
+  it('returns `false` for primitive value', () => {
+    expect(isIterable(1)).toBe(false);
+    expect(isIterable('abc')).toBe(false);
+  });
   it('returns `false` for function', () => {
     expect(isIterable(noop)).toBe(false);
   });
-  it('return `false` for object with `[Symbol.iterator]` property unset', () => {
-    expect(isIterable({ [Symbol.iterator]: null })).toBe(false);
+  it('return `false` for object without `[Symbol.iterator]` method', () => {
+    expect(isIterable({ [Symbol.iterator]: true })).toBe(false);
   });
 });
--- src/type/is-iterable.ts
+++ src/type/is-iterable.ts
@@ -1,12 +1,17 @@
+import { mayHaveProperties } from '../object/may-have-properties.js';
+
 /**
  * Checks whether the given object is iterable.
  *
  * @typeParam TElement - Iterable elements type.
- * @typeParam TOther - Non-iterable value type.
+ * @typeParam TOther - Non-iterable type the `value` may have.
  * @param value - An object value to check.
  *
- * @returns `true` if the `value` has a `[Symbol.iterator]` property set, or `false` otherwise.
+ * @returns `true` if the `value` has a `[Symbol.iterator]` method, or `false` otherwise.
  */
-export function isIterable<TElement, TOther>(value: Iterable<TElement> | TOther): value is Iterable<TElement> {
-  return !!(value as Iterable<TElement>)[Symbol.iterator];
+export function isIterable<TElement, TOther = unknown>(
+    value: Iterable<TElement> | TOther,
+): value is Iterable<TElement> {
+  return (/*#__INLINE__*/ mayHaveProperties(value))
+      && typeof (value as Partial<Iterable<TElement>>)[Symbol.iterator] === 'function';
 }
