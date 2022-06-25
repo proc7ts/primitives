179f8eb21e7db13f52e1ce1c846d736f7380034f
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-06-25 22:07:18 +0700

    build: utilize `@run-z/project-config`

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,35 +1,6 @@
-import { defineConfig } from 'rollup';
-import flatDts from 'rollup-plugin-flat-dts';
-import sourcemaps from 'rollup-plugin-sourcemaps';
-import ts from 'rollup-plugin-typescript2';
-import typescript from 'typescript';
+import { ProjectConfig } from '@run-z/project-config';
+import { ProjectRollup } from '@run-z/project-config/rollup';
 
-export default defineConfig({
-  input: {
-    primitives: './src/mod.ts',
-  },
-  plugins: [
-    ts({
-      typescript,
-      tsconfig: 'tsconfig.main.json',
-      cacheRoot: 'target/.rts2_cache',
-    }),
-    sourcemaps(),
-  ],
-  output: {
-    dir: './dist',
-    format: 'esm',
-    sourcemap: true,
-    entryFileNames: '[name].js',
-    plugins: [
-      flatDts({
-        tsconfig: 'tsconfig.main.json',
-        lib: true,
-        file: 'primitives.d.ts',
-        compilerOptions: {
-          declarationMap: true,
-        },
-      }),
-    ],
-  },
-});
+const project = new ProjectConfig();
+
+export default new ProjectRollup(project).build();
